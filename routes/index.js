5092dc27c25a09a1266935d3c943b5e960a9aa63
ljwedin <ljwedin@gmail.com>
2021-05-23 22:03:24 +0200

    Moves functions around and removes color picker to instead serve a random color to users

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const path = require('path');
-const { updateColors, sendColors } = require('../utils/colors.js');
+const { randomColor } = require('../utils/colors.js');
 
 // Gets available colors from colors array
-router.get('/pickColor', function (req, res) {
-    res.send(sendColors());
-})
-
-// Posts color from body and removes/adds to color array
-router.post('/pickColor', function (req, res) {
-    res.send(updateColors(req.body.color), sendColors());
+router.get('/getColor', function (req, res) {
+    res.send(randomColor());
 })
 
 module.exports = router;
