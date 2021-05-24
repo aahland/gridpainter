4e14a5b30990ddeef586d2749d3c768c9d09402e
Lisa Wedin <45404509+ljwedin@users.noreply.github.com>
2021-05-24 10:08:33 +0200

    Changes address from localhost to heroku server

--- public/javascripts/modules/randomColor.js
+++ public/javascripts/modules/randomColor.js
@@ -2,7 +2,7 @@ const socket = io();
 
 // Fetch function for recieving available random color
 async function getColor () {
-    let url = 'http://localhost:3000/getColor';
+    let url = 'http://gridpainter3.herokuapp.com/getColor';
     let response = await fetch(url, {
         method: 'GET',
         headers: {
