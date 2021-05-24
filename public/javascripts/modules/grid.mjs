5d1afb89d5d23f87124d4a67038662b6e99a10e2
Malin Johansson <malin.johansson@medieinstitutet.se>
2021-05-24 12:21:27 +0200

    saves white color to database

--- public/javascripts/modules/grid.mjs
+++ public/javascripts/modules/grid.mjs
@@ -131,33 +131,59 @@ export function gridColoringFunc(){
     let box=evt.target.id;
    
     let playerColor=localStorage.getItem("playerColor")
-    let positionColor = {"boxName":box, "boxColor":playerColor};
-    console.log(positionColor);
+    // let positionColor = {"boxName":box, "boxColor":playerColor};
+    // console.log(positionColor);
     
     if(document.getElementById(box).style.backgroundColor.match(playerColor)){
         console.log(playerColor);
         let backColor="white";
         console.log(backColor);
         document.getElementById(box).style.backgroundColor=backColor;
-        socket.emit("boxColor",{"boxName":box, "boxColor":backColor}); 
+        let positionColor = {"boxName":box, "boxColor":backColor};
+        socket.emit("boxColor",positionColor);
+
+        fetch("https://gridpainter3.herokuapp.com/users/color", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(positionColor)
+            })
+            .then(res=>res.json())
+            .then(paint=>{
+            console.log("Status",paint.status);
+            });
     }else{
             document.getElementById(box).style.backgroundColor=playerColor;
+            let positionColor = {"boxName":box, "boxColor":playerColor};
             socket.emit("boxColor",positionColor); 
+
+            fetch("https://gridpainter3.herokuapp.com/users/color", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(positionColor)
+                })
+                .then(res=>res.json())
+                .then(paint=>{
+                console.log("Status",paint.status);
+                }); 
     }   
-    fetch("https://gridpainter3.herokuapp.com/users/color", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        'Accept': 'application/json'
-    },
-    body: JSON.stringify(positionColor)
-    })
-    .then(res=>res.json())
-    .then(paint=>{
-    console.log("Status",paint.status);
-       
-       
-    });     
+    // fetch("https://gridpainter3.herokuapp.com/users/color", {
+    // method: "POST",
+    // headers: {
+    //     "Content-Type": "application/json",
+    //     'Accept': 'application/json'
+    // },
+    // body: JSON.stringify(positionColor)
+    // })
+    // .then(res=>res.json())
+    // .then(paint=>{
+    // console.log("Status",paint.status);
+    // });     
 }); 
 } 
 // reading data from database for other players to see which box is colored and realtime app 
