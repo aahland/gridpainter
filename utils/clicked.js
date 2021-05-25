ac0e5df0a9bde99d2ec006abadb416e1585e81e8
ljwedin <ljwedin@gmail.com>
2021-05-25 09:13:30 +0200

    Changes clicked util to reset counter at or above 4

--- utils/clicked.js
+++ utils/clicked.js
@@ -1,12 +1,13 @@
 let loginClicked = 0;
+
 function timesClicked(){
     loginClicked++
-    if (loginClicked == 4){
+    if (loginClicked >= 4){
+        loginClicked = 0;
         return true;
     } else {
         return false;
     }
-    
 }
 
 module.exports = timesClicked;
