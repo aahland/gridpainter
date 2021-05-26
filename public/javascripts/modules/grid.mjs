bf399cc9d6e5af3e06233a5c28c217be3ef67f07
Sara Sedaghat <75505464+Sarasedaghat8960@users.noreply.github.com>
2021-05-26 11:24:42 +0200

    change localhost till heroku

--- public/javascripts/modules/grid.mjs
+++ public/javascripts/modules/grid.mjs
@@ -8,7 +8,7 @@ const socket =io();
 let playerName=localStorage.getItem("playerName");
 let picture=localStorage.getItem("picture")
 
-let serverUrl = "http://localhost:3000";
+let serverUrl = "https://gridpainter3.herokuapp.com";
 
 //https://gridpainter3.herokuapp.com
 //http://localhost:3000
